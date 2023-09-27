ae2810f27f11987c8b754b7a5ccac9db452fead0
khaleu <khaleu.mancini@etec.sp.gov.br>
2023-09-27 17:51:23 -0300

    terminado o projeto

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	setlocale(LC_ALL, "Portuguese");
+	int n1;
+	int n2;
+	int op;
+	int cont;
+	int res;
+	cont = 1;
+while (cont==1) {
+		printf("\n-------------------------\n");
+		printf("------ CALCULADORA ------\n");
+		printf("-------------------------\n");
+		printf("Digite o primeiro número: ");
+		scanf("%d", &n1);
+		printf("Digite o segundo número: ");
+		scanf("%d", &n2);
+		printf("-------------------------\n");
+		printf("OPERACOES\nSoma[1]\nSubtração[2]\nMultiplicação[3]\nDivisão[4]\n: ");
+		scanf("%d", &op);
+		
+		switch(op){
+			case 1:
+				res=n1+n2;
+				break;
+			case 2:
+				res=n1-n2;
+				break;
+			case 3:
+				res=n1*n2;
+				break;
+			case 4:
+				res=n1/n2;
+				break;
+		}
+		if(op>4){
+			printf("\nDigite uma operação que exista");
+		}
+		else if(op<1){
+			printf("\nDigite uma operação que exista");
+		}
+		printf("\nO resultado da operação foi:%d", res);
+		printf("\nDeseja fazer outra operação:\nSim[1]\nNão[0]\n: ");
+		scanf("%d", &cont);
+	}
+	return 0;
+}
